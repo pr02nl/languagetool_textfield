a2a1eec77a5f8663db1a210df181d29b1d5c7b56
Paulo Braga <pr02nl@gmail.com>
2025-01-29 16:56:36 -0300

    feat: :sparkles: adicionado os campos contextMenuBuilder e textCapitalization, assim como os comentários para cada campo público.

--- lib/src/presentation/language_tool_text_field.dart
+++ lib/src/presentation/language_tool_text_field.dart
@@ -36,27 +36,102 @@ class LanguageToolTextField extends StatefulWidget {
   /// textAlign = [TextAlign.start] by default.
   final TextAlign textAlign;
 
+  /// Determine the capitalization behavior of the text field.
+  /// [TextCapitalization] = [TextCapitalization.none] by default.
+  final TextCapitalization textCapitalization;
+
+  /// A builder for the context menu of the text field.
+  ///
+  /// If provided, this builder will be used to create the context menu
+  /// that appears when the user long-presses on the text field.
+  final EditableTextContextMenuBuilder? contextMenuBuilder;
+
   /// Determine text Direction
   final TextDirection? textDirection;
 
+  /// Called when the user changes the text in the text field.
+  ///
+  /// The callback receives the new text as a `String` parameter.
   final ValueChanged<String>? onTextChange;
+
+  /// Called when the user submits the text in the text field.
+  ///
+  /// The callback receives the submitted text as a `String` parameter.
   final ValueChanged<String>? onTextSubmitted;
+
+  /// Called when the user taps on the text field.
+  ///
+  /// The callback receives no parameters.
   final VoidCallback? onTap;
+
+  /// Called when the user taps outside the text field.
+  ///
+  /// The callback receives no parameters.
   final TapRegionCallback? onTapOutside;
+
+  /// The action to perform when the user submits the text field.
+  ///
+  /// For example, [TextInputAction.done] will close the keyboard when the user
+  /// submits the text field.
   final TextInputAction? textInputAction;
+
+  /// The type of keyboard that this text field should display.
+  ///
+  /// For example, [TextInputType.text] will show a standard keyboard,
+  /// while [TextInputType.number] will show a keyboard with numbers.
   final TextInputType? keyboardType;
+
+  /// The color of the cursor in the text field.
   final Color? cursorColor;
+
+  /// Whether the text field should automatically
+  /// focus itself when it is displayed.
+  /// Defaults to `false`.
   final bool autoFocus;
+
+  /// The focus node to use for the text field.
+  ///
+  /// If null, a new focus node will be created.
   final FocusNode? focusNode;
+
+  /// The appearance of the keyboard.
+  ///
+  /// This determines the style of the keyboard that will be displayed when the
+  /// text field is focused. For example, [Brightness.light] will display
+  /// a light keyboard, while [Brightness.dark] will display a dark keyboard.
   final Brightness? keyboardAppearance;
+
+  /// Whether the text field should automatically correct the user's input
+  /// as they type.
+  /// When set to `true`, the text field will attempt to correct spelling and
+  /// grammar errors as the user types.
+  /// When set to `false`, the text field will not automatically correct
+  /// the user's input.
   final bool autocorrect;
+
+  /// Whether the text field is read-only.
+  ///
+  /// When set to `true`, the text field will not allow the user to edit its
+  /// contents.
+  /// When set to `false`, the text field will allow the user to edit its
+  /// contents.
   final bool readOnly;
+
+  /// The cursor that will be displayed when the user hovers over
+  /// the text field.
+  /// If null, the default cursor will be used.
   final MouseCursor? mouseCursor;
+
+  /// Whether the text field should be centered horizontally.
+  ///
+  /// When set to `true`, the text field will be centered horizontally.
+  /// When set to `false`, the text field will be left-aligned.
   final bool alignCenter;
 
   /// Creates a widget that checks grammar errors.
   const LanguageToolTextField({
     required this.controller,
+    this.textCapitalization = TextCapitalization.none,
     this.style,
     this.decoration = const InputDecoration(),
     this.language = 'auto',
@@ -81,6 +156,7 @@ class LanguageToolTextField extends StatefulWidget {
     this.onTextSubmitted,
     this.alignCenter = true,
     super.key,
+    this.contextMenuBuilder,
   });
 
   @override
@@ -126,6 +202,7 @@ class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
         );
 
         Widget childWidget = TextField(
+          textCapitalization: widget.textCapitalization,
           textAlign: widget.textAlign,
           textDirection: widget.textDirection,
           focusNode: _focusNode,
@@ -148,6 +225,7 @@ class _LanguageToolTextFieldState extends State<LanguageToolTextField> {
           onSubmitted: widget.onTextSubmitted,
           onTap: widget.onTap,
           onTapOutside: widget.onTapOutside,
+          contextMenuBuilder: widget.contextMenuBuilder,
         );
 
         if (widget.alignCenter) {
